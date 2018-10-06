9a91d88cdd8de2330944337ac5008b538b61933a
Jayphen <jayphen@gmail.com>
2018-10-06 12:34:40 +0200

    Remove semicolons ooo

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import http from "http";
+import http from "http"
 
-export const poop: string[] = ["hello my dude", "welcome to node"];
+export const poop: string[] = ["hello my dude", "welcome to node"]
 
 http
   .createServer((_: http.IncomingMessage, response: http.ServerResponse) => {
-    response.write(poop.map((msg: string) => msg.toUpperCase()).join("\n"));
-    response.end();
+    response.write(poop.map((msg: string) => msg.toUpperCase()).join("\n"))
+    response.end()
   })
-  .listen(3000);
+  .listen(3000)
